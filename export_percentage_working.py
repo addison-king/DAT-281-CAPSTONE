f9b0b0be0c4425a634add51fde61cabe4d82e111
brandyn-gilbert <58628433+brandyn-gilbert@users.noreply.github.com>
2021-05-18 20:34:15 -0400

    Update export_percentage_working.py

--- export_percentage_working.py
+++ export_percentage_working.py
@@ -21,26 +21,34 @@ def main(location):
     os.chdir(location)
     perc_working.to_csv(file_name, index=False, encoding='utf-8')
 
-    
+
 def percentage_working():
     query = '''SELECT Last_Contact.ID_number, currently_employed, graduation_year
                 FROM Last_Contact
                 INNER JOIN Basic_Info on Basic_Info.ID_number = Last_Contact.ID_number
                 ORDER BY graduation_year asc, currently_employed'''
-    connection = _db_connection()            
+    connection = _db_connection()
     data = pd.read_sql(query, con=connection)
     connection.close()
-    result = pd.DataFrame(columns=['Graduation Year', 'Not Working', 'Working', 
+    result = pd.DataFrame(columns=['Graduation Year', 'Not Working', 'Working',
                                    'Total', 'Percentage Working'])
 
     for i in data.graduation_year.unique():
         raw_data = data.loc[data['graduation_year'] == i].value_counts(subset='currently_employed')
 
-        no = raw_data['No']
-        yes = raw_data['Yes']
+        if 'Yes' in raw_data.keys():
+            yes = raw_data['Yes']
+        else:
+            yes = 0
+
+        if 'No' in raw_data.keys():
+            no = raw_data['No']
+        else:
+            no = 0
+
         total = no + yes
         perc_yes = round(yes/total, 3)
-        # print('Graduation Year: ', i, '\nNot working: ', no, '\nWorking: ', yes, 
+        # print('Graduation Year: ', i, '\nNot working: ', no, '\nWorking: ', yes,
         #       '\nTotal: ', total, '\nPercentage working: ', perc_yes, '%',
         #       '\n\n', sep='')
         dat = [i, no, yes, total, perc_yes]
@@ -51,8 +59,8 @@ def percentage_working():
     #     print('Percentage Working: ', round(result.at[i,'Percentage Working'], 1),'%' ,sep='')
     #     print()
     return result
-    
-    
+
+
 def _db_connection():
     '''
     Connects to the .db file
@@ -70,4 +78,6 @@ def _db_connection():
 
 
 if __name__ == "__main__":
+    # location = 'C:\\Users\\falconfoe\\Desktop\\Testing'
+    # main(location)
     main()
